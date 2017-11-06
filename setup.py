7a6c2d5a9a08e19b2e072195b49058067e2f0725
JuBra <jubra@bio.dtu.dk>
2017-11-06 22:53:36 +0100

    New version 0.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="GEMEditor",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     install_requires=['cobra',
                       'escher',
