5fb4600ae54ceb4d0c14bd1e5c21da64b8a3d59e
JuBra <jubra@bio.dtu.dk>
2017-11-21 18:52:23 +0100

    Fix test of get_yield

--- GEMEditor/solution/test/test_analysis.py
+++ GEMEditor/solution/test/test_analysis.py
@@ -30,35 +30,73 @@ def test_get_turnover():
 class TestGetYields:
 
     def test_get_yield(self):
-        model = Model("model")
-        met1 = Metabolite("m1", formula="H2O")
-        met2 = Metabolite("m2", formula="O2")
-        met3 = Metabolite("m3", formula="H2O2")
+        model = Model("test")
+        met1 = Metabolite(id="S7P", formula="C7H15O10P")
+        met2 = Metabolite(id="T3P1", formula="C3H7O6P")
+        met3 = Metabolite(id="E4P", formula="C4H9O7P")
+        met4 = Metabolite(id="F6P", formula="C6H13O9P")
+        model.add_metabolites((met1, met2, met3, met4))
 
-        model.add_metabolites((met1, met2, met3))
+        react1 = Reaction(id="r1", name="Transaldolase")
+        react1.add_metabolites({met1: -1, met2: -1, met3: 1, met4: 1})
 
-        react1 = Reaction("r1")
-        react1.add_metabolites({met1: -1})
+        react2 = Reaction(id="r2")
+        react2.add_metabolites({met1: -1})
 
-        react2 = Reaction("r2")
-        react2.add_metabolites({met2: -1})
+        react3 = Reaction(id="r3")
+        react3.add_metabolites({met2: -1})
 
-        react3 = Reaction("r3")
-        react3.add_metabolites({met1: -2,
-                                met2: -1,
-                                met3: 2})
-
-        react4 = Reaction("r4")
+        react4 = Reaction(id="r4")
         react4.add_metabolites({met3: -1})
 
-        model.add_reactions((react1, react2, react3, react4))
+        react5 = Reaction(id="r5")
+        react5.add_metabolites({met4: -1})
 
-        fluxes = {react1.id: -10,
-                  react2.id: -5,
-                  react3.id: 5,
-                  react4.id: 10}
+        model.add_reactions((react1, react2, react3, react4, react5))
+
+        fluxes = {react1.id: 1,
+                  react2.id: -1,
+                  react3.id: -1,
+                  react4.id: 1,
+                  react5.id: 1}
 
         status, yields = get_yields(fluxes, model)
-
         assert status is True
-        assert yields == {"H": {met3: 1.}, "O": {met3: 1.}}
+        assert yields == {"C": {met3: 0.4, met4: 0.6},
+                          "H": {met3: 9/22, met4: 13/22},
+                          "O": {met3: 7/16, met4: 9/16},
+                          "P": {met3: 0.5, met4: 0.5}}
+
+    def get_yield_not_working_if_input_formula_missing(self):
+        model = Model("test")
+        met1 = Metabolite(id="S7P")
+        met2 = Metabolite(id="T3P1", formula="C3H7O6P")
+        met3 = Metabolite(id="E4P", formula="C4H9O7P")
+        met4 = Metabolite(id="F6P", formula="C6H13O9P")
+        model.add_metabolites((met1, met2, met3, met4))
+
+        react1 = Reaction(id="r1", name="Transaldolase")
+        react1.add_metabolites({met1: -1, met2: -1, met3: 1, met4: 1})
+
+        react2 = Reaction(id="r2")
+        react2.add_metabolites({met1: -1})
+
+        react3 = Reaction(id="r3")
+        react3.add_metabolites({met2: -1})
+
+        react4 = Reaction(id="r4")
+        react4.add_metabolites({met3: -1})
+
+        react5 = Reaction(id="r5")
+        react5.add_metabolites({met4: -1})
+
+        model.add_reactions((react1, react2, react3, react4, react5))
+
+        fluxes = {react1.id: 1,
+                  react2.id: -1,
+                  react3.id: -1,
+                  react4.id: 1,
+                  react5.id: 1}
+
+        status, _ = get_yields(fluxes, model)
+        assert status is False
