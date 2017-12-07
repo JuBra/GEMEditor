92526682581daf384b4140922da8e429ffd75498
JuBra <jubra@bio.dtu.dk>
2017-12-07 11:23:12 +0100

    Remove facts rework annotation dialog

--- GEMEditor/model/edit/annotation.py
+++ GEMEditor/model/edit/annotation.py
@@ -1,89 +1,104 @@
 import re
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtWidgets import QDialog, QDialogButtonBox
-from GEMEditor.facts import Metabolite_annotations, Reaction_annotations, Gene_annotations, Resource
+from GEMEditor.database import get_options
 from GEMEditor.model.classes.annotation import Annotation
-from GEMEditor.model.classes.cobra import Reaction, Metabolite, Gene
 from GEMEditor.model.edit.ui.EditAnnotationsDialog import Ui_EditAnnotationDialog
 
 
 class EditAnnotationDialog(QDialog, Ui_EditAnnotationDialog):
 
-    def __init__(self, annotation=None, item=None):
+    def __init__(self, item, annotation=None):
         super(EditAnnotationDialog, self).__init__()
         self.setupUi(self)
-        self.annotation = None
-        self.item = None
-        self.lookup = {}
 
         # Store status images
         self.status_unknown = QtGui.QPixmap(":/status_undefined")
         self.status_okay = QtGui.QPixmap(":/status_okay")
         self.status_error = QtGui.QPixmap(":/status_error")
 
-        self.set_annotation(annotation, item)
-        self.setup_links()
+        # Store validators
+        self._validators = dict()
+        self._collections = dict()
 
-    def setup_links(self):
+        self.set_annotation(annotation, item)
+        self._setup_signals()
+
+    def _setup_signals(self):
         self.annotationLineEdit.textChanged.connect(self.validate_annotation)
         self.typeComboBox.currentTextChanged.connect(self.validate_annotation)
 
     def set_annotation(self, annotation, item):
         """ Populate the display according to the information provided """
 
-        self.annotation = annotation
-        self.item = item
+        databases = get_options(item.__class__.__name__)
+        self._update_mappings(databases)
 
-        self.set_options(item)
+        # Get options for user to select from
+        options = dict((x.miriam_collection, x.name) for x in databases)
+        if annotation and annotation.collection not in options:
+            options[annotation.collection] = annotation.collection
+
+        # Add options to combobox
+        entries = sorted(options.values())
+        self.typeComboBox.clear()
+        self.typeComboBox.addItems(entries)
+
+        # Update widgets from annotation
         if annotation:
-            try:
-                display_name = self.lookup[annotation.collection].display_name
-            except KeyError:
-                self.lookup[annotation.collection] = Resource(annotation.collection, annotation.collection, r".*")
-                self.typeComboBox.addItem(annotation.collection)
-                display_name = annotation.collection
-            finally:
-                index = self.typeComboBox.findText(display_name)
-                self.annotationLineEdit.setText(annotation.identifier)
-        else:
-            index = 0
-        self.typeComboBox.setCurrentIndex(index)
+            self.typeComboBox.setCurrentIndex(entries.index(options[annotation.collection]))
+            self.annotationLineEdit.setText(annotation.identifier)
+
         self.validate_annotation()
 
-    def set_options(self, item):
-        self.typeComboBox.clear()
-        self.lookup.clear()
+    def _update_mappings(self, databases):
+        """ Update the mappings with relevant information
 
-        if isinstance(item, Reaction):
-            self.lookup.update(Reaction_annotations)
-        elif isinstance(item, Metabolite):
-            self.lookup.update(Metabolite_annotations)
-        elif isinstance(item, Gene):
-            self.lookup.update(Gene_annotations)
+        Parameters
+        ----------
+        databases: list,
+            The list containing the relevant database entries
+
+        """
+        self._validators.clear()
+        self._validators.update((x.miriam_collection, x.validator) for x in databases)
+
+        self._collections.clear()
+        self._collections.update((x.name, x.miriam_collection) for x in databases)
+
+    def _get_collection(self):
+        """ Return the current database choice
+
+        Returns
+        -------
+        str:
+            The currently selected collection
+
+        """
+        choice = self.typeComboBox.currentText()
+        if choice in self._collections:
+            return self._collections[choice]
         else:
-            return
-
-        self.typeComboBox.addItems(sorted([x.display_name for x in set(self.lookup.values())]))
+            return choice
 
     @QtCore.pyqtSlot()
     def validate_annotation(self):
-        text = self.annotationLineEdit.text()
-        try:
-            pattern = self.lookup[self.typeComboBox.currentText()].validator
-        except KeyError:
-            # Unknown status
-            self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
+        identifier = self.annotationLineEdit.text()
+        collection = self._get_collection()
+        if collection not in self._validators:
+            # Unknown collection
+            self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(True)
             self.statusLabel.setPixmap(self.status_unknown)
+        elif identifier and re.match(self._validators[collection], identifier):
+            # Valid identifier
+            self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(True)
+            self.statusLabel.setPixmap(self.status_okay)
         else:
-            if text and re.match(pattern, text):
-                self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(True)
-                self.statusLabel.setPixmap(self.status_okay)
-            else:
-                self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
-                self.statusLabel.setPixmap(self.status_error)
+            # Empty or invalid id
+            self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(False)
+            self.statusLabel.setPixmap(self.status_error)
 
     def get_annotation(self):
         """ Return an annotation object from the user input """
-        text = self.typeComboBox.currentText()
-        return Annotation(collection=self.lookup[text].collection,
+        return Annotation(collection=self._get_collection(),
                           identifier=self.annotationLineEdit.text())
