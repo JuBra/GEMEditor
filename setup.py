e1d95f07efac2d72f888937ec8d2d305ec1f81bc
JuBra <JuBra@users.noreply.github.com>
2018-01-24 17:51:38 +0100

    New version 0.3.4 (#48)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="GEMEditor",
-    version="0.3.3",
+    version="0.3.4",
     packages=find_packages(),
     install_requires=['cobra',
                       'escher',
