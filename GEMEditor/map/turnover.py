bb3c0580201e562b105396c5b16ac4ed48eebaa1
JuBra <jubra@bio.dtu.dk>
2018-01-31 23:34:59 +0100

    Clean up turnover

--- GEMEditor/map/turnover.py
+++ GEMEditor/map/turnover.py
@@ -23,7 +23,6 @@ def prev_reaction_shift(n, params):
     params: dict,
         Plotting parameters
 
-
     Returns
     -------
     np.array:
@@ -63,23 +62,50 @@ def middle_node_positions(origin, params):
     return top, middle, bottom
 
 
-def metabolite_positions(origin, n, params):
+def metabolite_positions(origin, metabolites, params):
+    """ Calculate the positions of the metabolites
 
-    positions = []
+    Parameters
+    ----------
+    origin: np.array,
+        Origin of the reaction
+    metabolites: iterable,
+        Metabolites to position
+    params: dict,
+        Layout parameters
 
-    if n == 0:
-        pass
+    Returns
+    -------
+    positions: list,
+        Positions of the metabolites
+    """
+
+    n = len(metabolites)
+
+    if not metabolites:
+        return []
     elif n == 1:
-        positions.append(origin + np.array((params["reaction_width"] / 2, 0)))
+        return [origin + np.array((params["reaction_width"] / 2, 0))]
     else:
         step = params["reaction_width"] / (n - 1)
-        for i in range(n):
-            positions.append(origin + i * np.array((step, 0)))
-
-    return positions
+        return [origin + i * np.array((step, 0)) for i in range(n)]
 
 
-def total_width_reactions(n, params):
+def reaction_box_width(n, params):
+    """ Calculate the width of the boxes containing reactions
+
+    Parameters
+    ----------
+    n: int,
+        Number of reactions in the box
+    params: dict,
+        Layout parameters
+
+    Returns
+    -------
+    float or int,
+        Width of the reaction box
+    """
 
     if n == 0:
         return 0
@@ -88,14 +114,33 @@ def total_width_reactions(n, params):
         return (n * width) + ((n-1) * params["reaction_x_padding"])
 
 
-def centering_shift(num_producing, num_consuming, params):
+def centering(num_producing, num_consuming, params):
+    """ Calculate the centering shift
 
-    width_producing = total_width_reactions(num_producing, params)
-    width_consuming = total_width_reactions(num_consuming, params)
+    Parameters
+    ----------
+    num_producing: int,
+        Number of producing reactions
+    num_consuming: int,
+        Number of consuming reactions
+    params: dict,
+        Layout parameters
 
-    shift = abs(width_producing-width_consuming)/2
+    Returns
+    -------
+    np.array,
+        Shift of the producing reactions
+    np.array,
+        Shift of the consuming reactions
+    """
 
-    if width_producing > width_consuming:
+    # Calculate the width of the boxes containing reactions
+    producing = reaction_box_width(num_producing, params)
+    consuming = reaction_box_width(num_consuming, params)
+
+    shift = abs(producing-consuming)/2
+
+    if producing > consuming:
         # Shift consuming
         return np.array((0, 0)), np.array((shift, 0))
     else:
@@ -105,10 +150,10 @@ def centering_shift(num_producing, num_consuming, params):
 
 def add_metabolites(graph, positions, origin, reaction, metabolites, subnode, params):
 
-    met_positions = metabolite_positions(origin, len(metabolites), params)
+    met_positions = metabolite_positions(origin, metabolites, params)
 
-    for i, m in enumerate(metabolites):
-        node = (reaction, m)
+    for i, metabolite in enumerate(metabolites):
+        node = (reaction, metabolite)
         graph.add_node(node)
         graph.add_edge(subnode, node)
         positions[node] = met_positions[i]
@@ -150,44 +195,108 @@ def add_reaction(graph, positions, origin, reaction, metabolite, met_connection_
         add_metabolites(graph, positions, origin, reaction, other.keys(), top_node, params)
 
 
-def get_margin_shift(params):
+def margin_shift(params):
+    """ Get the shift in positions by margin
+
+    Parameters
+    ----------
+    params: dict,
+        Layout parameters
+
+    Returns
+    -------
+    np.array,
+        Margin shift
+    """
+
     return np.array((params["x_margin"], params["y_margin"]))
 
 
-def position_center(n, params):
-    x_shift = np.array((total_width_reactions(n, params), 0)) / 2
+def center(n, params):
+    """ Get center position
+
+    Parameters
+    ----------
+    n: int,
+        Number of consuming reactions or producing reactions
+    params: dict,
+        Layout parameters
+
+    Returns
+    -------
+    np.array,
+        Center position
+
+    """
+
+    x_shift = np.array((reaction_box_width(n, params), 0)) / 2
     y_shift = np.array((0, params["reaction_height"] + params["center_y_padding"]))
 
-    return get_margin_shift(params) + x_shift + y_shift
+    return margin_shift(params) + x_shift + y_shift
 
 
 def layout_turnover(metabolite, rates, params):
+    """ Calculate coordinates of the individual nodes
+
+    Parameters
+    ----------
+    metabolite: GEMEditor.main.classes.Metabolite,
+        Metabolite for which to generate the map
+    rates: dict,
+        Dictionary containing the partial rates of the reactions
+    params: dict,
+        Layout parameters
+
+    Returns
+    -------
+    graph: MapGraph,
+        Graph containing nodes and edges
+    positions: dict,
+        Dictionary containing positions of the nodes
+
+    """
 
     graph = MapGraph()
     positions = {}
-
     producing, consuming, _ = split_dict_by_value(rates)
 
-    margin_shift = get_margin_shift(params)
-    prod_x_shift, cons_x_shift = centering_shift(len(producing), len(consuming), params)
-    cons_y_shift = np.array((0, 2 * params["center_y_padding"] + params["reaction_height"]))
+    # Calculate the shift from position (0,0)
+    margin = margin_shift(params)
+    # Calculate centering of reactions
+    prod_centering, cons_centering = centering(len(producing), len(consuming), params)
+    # Calculate consuming y_offset
+    cons_y_offset = np.array((0, 2 * params["center_y_padding"] + params["reaction_height"]))
 
-    positions[metabolite] = position_center(max(len(producing), len(consuming)), params)
+    positions[metabolite] = center(max(len(producing), len(consuming)), params)
 
     for i, item in enumerate(sorted(producing.items(), key=lambda x: x[1], reverse=True)):
-        origin = margin_shift + prod_x_shift + prev_reaction_shift(i, params)
+        origin = margin + prod_centering + prev_reaction_shift(i, params)
         add_reaction(graph, positions, origin, item[0], metabolite, "bottom", params)
 
     for i, item in enumerate(sorted(consuming.items(), key=lambda x: abs(x[1]), reverse=True)):
-        origin = margin_shift + cons_x_shift + prev_reaction_shift(i, params) + cons_y_shift
+        origin = margin + cons_centering + prev_reaction_shift(i, params) + cons_y_offset
         add_reaction(graph, positions, origin, item[0], metabolite, "top", params)
 
     return graph, {k: tuple(v) for k, v in positions.items()}
 
 
 def setup_turnover_map(metabolite, rates, params=PARAMS_TURNOVER):
+    """ Generate metabolite turnover map
+
+    Parameters
+    ----------
+    metabolite: GEMEditor.main.classes.Metabolite,
+        Metabolite for which to generate the map
+    rates: dict,
+        Dictionary containing the partial rates of the reactions
+    params: dict,
+        Layout parameters
+
+    Returns
+    -------
+    str,
+        Escher json string
+    """
+
     graph, pos = layout_turnover(metabolite, rates, params)
-
-    positive, negative, _ = split_dict_by_value(rates)
-
     return get_escher_json(graph, pos, params)
