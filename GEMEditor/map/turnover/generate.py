32e55722f49d078a163090c4e6de56bc697d590d
JuBra <jubra@bio.dtu.dk>
2018-01-31 19:36:32 +0100

    Move base functions to base submodule

--- GEMEditor/map/turnover/generate.py
+++ GEMEditor/map/turnover/generate.py
@@ -1,7 +1,5 @@
-import networkx as nx
 import numpy as np
-from cobra import Metabolite
-import json
+from GEMEditor.map.base import get_escher_json, MapGraph, add_subnodes
 from GEMEditor.base import split_dict_by_value
 
 
@@ -13,177 +11,6 @@ PARAMS_TURNOVER = {
     "reaction_x_padding": 100,
     "center_y_padding": 600,
     "met_y_offset": 100}
-
-
-class MapGraph(nx.Graph):
-
-    def __init__(self, *args, **kwargs):
-        super(MapGraph, self).__init__(*args, **kwargs)
-
-    @property
-    def reactions(self):
-        return set(t[0] for t in self.nodes() if isinstance(t, tuple))
-
-
-def get_subnodes(reaction):
-    """ Get all the subnodes for a reaction
-
-    Parameters
-    ----------
-    reaction :
-
-    Returns
-    -------
-
-    """
-    return (reaction, "educts"), (reaction, "middle"), (reaction, "products")
-
-
-def entry_from_metabolite_node(node, x, y):
-    """ Generate a metabolite entry for use in Escher map
-
-    Nodes are expected to be of the form:
-    <Metabolite>
-    (<Reaction>, <Metabolite>)
-    (<Reaction>, "middle")
-    (<Reaction>, "educts")
-    (<Reaction>, "products")
-
-
-    Parameters
-    ----------
-    node: Metabolite or tuple,
-        The node for which to generate an entry
-    x: float or int,
-        x position of the metabolite on the map
-    y: float or int,
-        y position of the metabolite on the map
-
-    Returns
-    -------
-    dict,
-        Metabolite entry
-    """
-
-    entry = {"x": x, "y": y}
-
-    def add_metabolite_info(metabolite, is_primary):
-        label_offset = 25 if is_primary else 10
-        entry.update({"bigg_id": metabolite.id, "name": metabolite.name,
-                      "label_x": x + label_offset, "label_y": y + label_offset,
-                      "node_is_primary": is_primary, "node_type": "metabolite"})
-
-    # Add appropriate information
-    if isinstance(node, Metabolite):
-        add_metabolite_info(node, is_primary=True)
-    elif isinstance(node[1], Metabolite):
-        add_metabolite_info(node[1], is_primary=False)
-    elif node[1] == "middle":
-        entry["node_type"] = "midmarker"
-    else:
-        entry["node_type"] = "multimarker"
-
-    return entry
-
-
-def entry_from_reaction(graph, reaction, node_index, positions, counter):
-
-    segments = {}
-    json_metabolites = []
-    educt_node, middle_node, product_node = get_subnodes(reaction)
-
-    # Get the middle node of the reaction
-    middle_node_id = node_index[middle_node]
-    x, y = positions[middle_node]
-
-    for metabolite, stoichiometry in reaction.metabolites.items():
-        json_metabolites.append({"coefficient": stoichiometry, "bigg_id": metabolite.id})
-
-        for node in (metabolite, (reaction, metabolite)):
-            for edge in graph.edges([node]):
-                node1, node2 = edge
-                segments[counter()] = {"from_node_id": node_index[node1], "to_node_id": node_index[node2], "b1": None, "b2": None}
-
-    # Connect intermediate nodes to middle node
-    for intermediate_node in (educt_node, product_node):
-        if intermediate_node in node_index:
-            segments[counter()] = {"from_node_id": node_index[intermediate_node],
-                                   "to_node_id": middle_node_id, "b1": None, "b2": None}
-
-    return {"name": reaction.name or reaction.id,
-            "bigg_id": reaction.id,
-            "reversibility": reaction.lower_bound < 0. < reaction.upper_bound,
-            "label_x": x+10,
-            "label_y": y+10,
-            "gene_reaction_rule": reaction.gene_reaction_rule,
-            "genes": [dict([("bigg_id", x.id), ("name", x.name)]) for x in reaction.genes],
-            "metabolites": json_metabolites,
-            "segments": segments}
-
-
-def get_escher_json(graph, positions, params):
-
-
-    # Generate unique numeric ids
-    class Counter:
-
-        def __init__(self):
-            self.count = -1
-
-        def __call__(self, *args, **kwargs):
-            self.count += 1
-            return str(self.count)
-
-    counter = Counter()
-
-    result = [{"map_name": "test_name",
-               "map_id": "1234565",
-               "map_description": "test",
-               "homepage": "https://escher.github.io",
-               "schema": "https://escher.github.io/escher/jsonschema/1-0-0#"}]
-
-    nodes = {}
-    node_index = {}
-
-    for node in graph.nodes():
-        index = counter()
-        node_index[node] = index
-
-        x, y = positions[node]
-        nodes[index] = entry_from_metabolite_node(node, x, y)
-
-    reactions_dict = {}
-    for reaction in graph.reactions:
-        reactions_dict[counter()] = entry_from_reaction(graph, reaction, node_index, positions, counter)
-
-    width, height = canvas_size(positions, params)
-    result.append({"reactions": reactions_dict, "nodes": nodes, "text_labels": {},
-                   "canvas": {"x": 0., "y": 0., "width": width, "height": height}})
-
-    return json.dumps(result)
-
-
-def add_subnodes(graph, reaction):
-    """ NetworkX graph
-
-    Parameters
-    ----------
-    graph: networkx.Graph,
-        Graph containing the map nodes
-    reaction:
-        Reaction to be plotted
-
-    Returns
-    -------
-
-    """
-
-    educt_node, middle_node, product_node = get_subnodes(reaction)
-    graph.add_nodes_from([educt_node, middle_node, product_node])
-    graph.add_edge(educt_node, middle_node)
-    graph.add_edge(middle_node, product_node)
-
-    return educt_node, middle_node, product_node
 
 
 def prev_reaction_shift(n, params):
@@ -356,12 +183,6 @@ def layout_turnover(metabolite, rates, params):
         add_reaction(graph, positions, origin, item[0], metabolite, "top", params)
 
     return graph, {k: tuple(v) for k, v in positions.items()}
-
-
-def canvas_size(pos, params):
-    width = max(p[0] for p in pos.values()) + 2 * params["x_margin"]
-    height = max(p[1] for p in pos.values()) + 2 * params["y_margin"]
-    return width, height
 
 
 def setup_turnover_map(metabolite, rates, params=PARAMS_TURNOVER):
